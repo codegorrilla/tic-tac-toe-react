b47336a3e91984688a9feed94efeacab0d185507
Sanjib Adhya <sanjib@zacksresearch.in>
2025-03-28 08:19:19 -0500

    completed player  name editor

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Player from './components/Player';
+
+export default function App() {
+	return (
+		<main>
+			<div id='game-container'>
+				<ol id='players'>
+					<Player
+						initialName='Player 1'
+						symbol='X'
+					/>
+					<Player
+						initialName='Player 2'
+						symbol='O'
+					/>
+				</ol>
+			</div>
+		</main>
+	);
+}
---
+++ src/components/Player.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+
+export default function Player({ initialName, symbol }) {
+	const [isEditing, setIsEditing] = useState(false);
+	const [updatedPlayerName, setUpdatedPlayerName] = useState(initialName);
+
+	let playerName = <span className='player-name'>{updatedPlayerName}</span>;
+
+	let btnCaption = <span>Edit</span>;
+
+	const handleEditClick = () => {
+		setIsEditing((editing) => !editing);
+		console.log(isEditing);
+	};
+
+	const handlePlayerName = (e) => {
+		setUpdatedPlayerName(e.target.value);
+	};
+
+	if (isEditing) {
+		playerName = (
+			<input
+				type='text'
+				value={updatedPlayerName}
+				onChange={handlePlayerName}
+				required
+			/>
+		);
+	}
+
+	if (isEditing) {
+		btnCaption = <span>Save</span>;
+	}
+
+	return (
+		<>
+			<span className='player'>
+				{playerName}
+				<span className='player-symbol'>{symbol}</span>
+			</span>
+			<button onClick={handleEditClick}>{btnCaption}</button>
+		</>
+	);
+}
